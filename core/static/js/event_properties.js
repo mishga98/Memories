bc53d134944f4a847e7e1c370245da43e333e07b
mishga98 <mishga98@gmail.com>
2020-12-16 00:03:23 +0700

    Add form for creating notes
    Form contents two fields and map

---
+++ core/static/js/event_properties.js
@@ -0,0 +1,46 @@
+ymaps.ready(init);
+var myMap;
+
+function init () {
+    myMap = new ymaps.Map("map", {
+        center: [56.0071, 92.8601], // Красноярск
+        zoom: 11
+    }, {
+        balloonMaxWidth: 200,
+        searchControlProvider: 'yandex#search'
+    });
+
+    // Обработка события, возникающего при щелчке
+    // левой кнопкой мыши в любой точке карты.
+    // При возникновении такого события откроем балун.
+    myMap.events.add('click', function (e) {
+        if (!myMap.balloon.isOpen()) {
+            var coords = e.get('coords');
+            myMap.balloon.open(coords, {
+                contentHeader:'Отлично!',
+                contentBody:'<p>Теперь сохраните заметку</p>' +
+                    '<p>Координаты: ' + [
+                    coords[0].toPrecision(6),
+                    coords[1].toPrecision(6)
+                    ].join(', ') + '</p>',
+                contentFooter:'<sup>Или вы можете выбрать другое место</sup>'
+            });
+        }
+        else {
+            myMap.balloon.close();
+        }
+    });
+
+    // Обработка события, возникающего при щелчке
+    // правой кнопки мыши в любой точке карты.
+    // При возникновении такого события покажем всплывающую подсказку
+    // в точке щелчка.
+    myMap.events.add('contextmenu', function (e) {
+        myMap.hint.open(e.get('coords'), 'Кто-то щелкнул правой кнопкой');
+    });
+    
+    // Скрываем хинт при открытии балуна.
+    myMap.events.add('balloonopen', function (e) {
+        myMap.hint.close();
+    });
+}
