9964c04d8906a2c8ef5f8b98767dc48bc8272a84
mishga98 <mishga98@gmail.com>
2020-12-15 22:10:32 +0700

    Create database to save notes
    DB has following fields: Title, note (description),
    coordinates to save coordinates and person (who is owner of this note)

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField('Название', max_length=72)
+    note = models.TextField('Описание')
+    coordinates = models.CharField('Координаты', max_length=72)
+    person = models.CharField('Автор', max_length=72)
+
+
+    def __str__(self):
+        return self.title
