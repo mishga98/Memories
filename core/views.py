a8dc93025a5f1b6ecd4d8852209d63a67f153e8d
mishga98 <mishga98@gmail.com>
2020-12-16 15:57:25 +0700

    cleaning up

--- core/views.py
+++ core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
 from .models import Note
 from .forms import NoteForm
+
+
 def index(request):
     return render(request, 'core/index.html')
+
 
 def notes(request):
     if str(request.user) == 'AnonymousUser':
@@ -13,16 +14,15 @@ def notes(request):
 
     if request.method == 'POST':
         request.POST = request.POST.copy()
-        request.POST['person']=request.user
+        request.POST['person'] = request.user
         form = NoteForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('notes')
     form = NoteForm()
-    notes = Note.objects.filter(person=request.user).order_by('-id')
+    mynotes = Note.objects.filter(person=request.user).order_by('-id')
     context = {
         'form': form,
-        'notes': notes
+        'notes': mynotes
     }
     return render(request, 'core/notes.html', context)
-
